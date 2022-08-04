a38b857295064b8f4c79b1ab8cca50d0beef6ea2
BeyondLogan <103182857+BeyondLogan@users.noreply.github.com>
2022-08-04 12:10:39 +0930

    Create SetUp.sh

---
+++ SetUp.sh
@@ -0,0 +1,21 @@
+sudo apt-get Update
+
+Sudo apt update
+
+sudo apt-get upgrade
+
+sudo apt upgrade
+
+sudo apt-get install curl
+
+wget -O installer.tar.gz https://github.com/easy-wi/installer/archive/3.1.tar.gz
+
+tar zxf installer.tar.gz && mv ./installer-*/easy-wi_install.sh ./
+
+sudo tar zxf installer.tar.gz && mv ./installer-*/easy-wi_install.sh ./
+
+rm -r installer.tar.gz installer-*/
+
+sudo rm -r installer.tar.gz installer-*/
+
+sudo bash ./easy-wi_install.sh
