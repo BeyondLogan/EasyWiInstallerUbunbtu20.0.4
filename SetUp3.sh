8c5a5fc804cf4941c81f60106a7eb60b93e39a6e
BeyondLogan <103182857+BeyondLogan@users.noreply.github.com>
2022-08-04 12:20:25 +0930

    Create SetUp3.sh

---
+++ SetUp3.sh
@@ -0,0 +1,21 @@
+sudo bash ./easy-wi_install.sh
+
+nano /etc/ssh/sshd_config
+
+/etc/init.d/ssh restart
+
+ls -la /home/easy-wi/
+insgesamt 124
+drwxr-xr-x 10 easy-wi easy-wi  4096 13. Jul 13:12 .
+drwxr-xr-x  5 root    root     4096 13. Jul 13:12 ..
+drwxr-xr-x  2 easy-wi easy-wi  4096 13. Jul 13:12 conf
+-rwxrwx---  1 easy-wi easy-wi 79656 13. Jul 12:35 control
+drwxrwx---  3 easy-wi easy-wi  4096 13. Jul 12:56 fdl_data
+drwxrwx---  2 easy-wi easy-wi  4096 13. Jul 12:56 logs
+drwxr-xr-x  2 easy-wi easy-wi  4096 13. Jul 12:56 masteraddons
+drwxr-xr-x  2 easy-wi easy-wi  4096 13. Jul 12:56 mastermaps
+drwxr-xr-x  2 easy-wi easy-wi  4096 13. Jul 13:12 masterserver
+drwxr-x---  2 easy-wi easy-wi  4096 13. Jul 13:12 .steam
+drwxrwx---  2 easy-wi easy-wi  4096 13. Jul 12:56 temp
+
+sudo reboot
