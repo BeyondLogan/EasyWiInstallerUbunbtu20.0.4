aac76b8b0ac96861144cd3830707fc8a11fc5cf7
BeyondLogan <103182857+BeyondLogan@users.noreply.github.com>
2022-08-04 12:17:37 +0930

    Create SetUp2.sh

---
+++ SetUp2.sh
@@ -0,0 +1,11 @@
+nano /etc/crontab
+
+0 */1 * * * easywi_web cd /home/easywi_web/htdocs && timeout 300 php ./reboot.php >/dev/null 2>&1
+*/5 * * * * easywi_web cd /home/easywi_web/htdocs && timeout 290 php ./statuscheck.php >/dev/null 2>&1
+*/1 * * * * easywi_web cd /home/easywi_web/htdocs && timeout 290 php ./startupdates.php >/dev/null 2>&1
+*/5 * * * * easywi_web cd /home/easywi_web/htdocs && timeout 290 php ./jobs.php >/dev/null 2>&1
+*/10 * * * * easywi_web cd /home/easywi_web/htdocs && timeout 290 php ./cloud.php >/dev/null 2>&1
+
+/etc/init.d/cron restart
+
+sudo reboot
