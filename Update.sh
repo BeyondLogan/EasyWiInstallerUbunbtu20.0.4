a0af72ca2cf465d1162db863c9915466a099692f
BeyondLogan <103182857+BeyondLogan@users.noreply.github.com>
2022-08-04 12:00:27 +0930

    Create Update.sh

---
+++ Update.sh
@@ -0,0 +1,29 @@
+sudo apt update
+
+sudo apt-get update
+
+sudo apt upgrade
+
+sudo apt-get upgrade
+
+sudo apt-get install curl
+
+sudo apt-get install wget
+
+sudo apt-get install git
+
+sudo apt-get install git-all
+
+sudo apt-get install gparted
+
+sudo apt-get install geparted
+
+sudo apt-get update
+
+sudo apt-get upgrade
+
+sudo apt update
+
+sudo apt upgrade
+
+sudo reboot
